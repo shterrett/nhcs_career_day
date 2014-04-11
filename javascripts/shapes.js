e2be2ed375a8532ca0ba83b37d435a1ffc3302f8
Stuart Terrett <shterrett@gmail.com>
2014-04-10 22:20:18 -0400

    Fix javascript formatting

--- javascripts/shapes.js
+++ javascripts/shapes.js
@@ -1,5 +1,5 @@
-$(document).ready(function () {
-  var shapeDemo = function(){
+$(document).ready(function() {
+  var shapeDemo = function() {
     var canvas = this.__canvas = new fabric.Canvas('c');
 
     var rectOptions = { width: 250,
@@ -20,7 +20,7 @@ $(document).ready(function () {
 
     var optionKeys = ['width', 'height', 'left', 'top', 'fill']
 
-    var mergeOptions = function () {
+    var mergeOptions = function() {
       suppliedOptions = userOptions()
       resultOptions = {}
       optionKeys.forEach(function(value, index) {
@@ -33,15 +33,15 @@ $(document).ready(function () {
       return resultOptions;
     }
 
-    var buildRectangle = function () {
+    var buildRectangle = function() {
       return new fabric.Rect(mergeOptions());
     }
 
-    $('button#render').on('click', function () {
+    $('button#render').on('click', function() {
       canvas.add(buildRectangle());
     });
 
-    $('button#clear').on('click', function () {
+    $('button#clear').on('click', function() {
       canvas.clear();
     });
   }
